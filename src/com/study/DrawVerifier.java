dfd1f25ec0e9992f5d6cea21aae04960f2349718
IlyaBuldakov <ggpanguin@gmail.com>
2021-10-23 00:55:58 +0400

    Add game class with child components

--- src/com/study/DrawVerifier.java
+++ src/com/study/DrawVerifier.java
@@ -19,10 +19,9 @@ package com.study;
 /**
  * * @author study
  */
-public final class Launcher {
-    public static void main(final String[] args) {
-        Game game = new Game(
-                new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier());
-        game.play();
+public class DrawVerifier {
+
+    public boolean isDraw(final GameTable gameTable) {
+        return false;
     }
 }
--- src/com/study/WinnerVerifier.java
+++ src/com/study/WinnerVerifier.java
@@ -19,10 +19,13 @@ package com.study;
 /**
  * * @author study
  */
-public final class Launcher {
-    public static void main(final String[] args) {
-        Game game = new Game(
-                new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier());
-        game.play();
+public class WinnerVerifier {
+
+    public boolean isUserWin(final GameTable gameTable) {
+        return false;
+    }
+
+    public boolean isComputerWin(final GameTable gameTable) {
+        return false;
     }
 }
