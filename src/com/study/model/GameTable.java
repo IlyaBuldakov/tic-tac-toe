e305ee11d8c3f079228bd40a7b914ec13f4f0b32
IlyaBuldakov <ggpanguin@gmail.com>
2021-10-23 11:03:44 +0400

    Override toString methods for model classes

--- src/com/study/model/GameTable.java
+++ src/com/study/model/GameTable.java
@@ -16,6 +16,8 @@
 
 package com.study.model;
 
+import java.util.Arrays;
+
 /**
  * * @author study
  */
@@ -36,5 +38,17 @@ public class GameTable {
 
     public void setSign(final Cell cell, final char sign) {
         table[cell.getRow()][cell.getCol()] = sign;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameTable{");
+        sb.append("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if (i < table.length - 1) sb.append(";");
+        }
+        sb.append('}');
+        return sb.toString();
     }
 }
