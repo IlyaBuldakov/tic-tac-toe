771cc3492c81d4c989b9a2df1260cbb0c07e2a75
IlyaBuldakov <ggpanguin@gmail.com>
2021-10-23 11:46:37 +0400

    Implement DataPrinter component

--- src/com/study/component/DataPrinter.java
+++ src/com/study/component/DataPrinter.java
@@ -16,6 +16,7 @@
 
 package com.study.component;
 
+import com.study.model.Cell;
 import com.study.model.GameTable;
 
 /**
@@ -24,10 +25,23 @@ import com.study.model.GameTable;
 public class DataPrinter {
 
     public void printMappingTable() {
-
+        System.out.println("-------------\n" +
+                "| 7 | 8 | 9 |\n" +
+                "-------------\n" +
+                "| 4 | 5 | 6 |\n" +
+                "-------------\n" +
+                "| 1 | 2 | 3 |\n" +
+                "-------------\n");
     }
 
     public void printGameTable(final GameTable gameTable) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
