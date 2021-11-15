6d98a3215ff0649065f8fe46a54404c830ffad49
IlyaBuldakov <ggpanguin@gmail.com>
2021-11-15 09:55:06 +0400

    Convert to maven structure

---
+++ src/main/java/com/study/Launcher.java
@@ -0,0 +1,33 @@
+/*
+ *    Copyright 2021. Study
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.study;
+
+import com.study.component.*;
+import com.study.component.keypad.TerminalNumericKeypadCellNumberConverter;
+
+/**
+ * * @author study
+ */
+public final class Launcher {
+    public static void main(final String[] args) {
+        final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
+
+        Game game = new Game(
+                new DataPrinter(cellNumberConverter), new ComputerMove(), new UserMove(cellNumberConverter), new WinnerVerifier(), new CellVerifier());
+        game.play();
+    }
+}
---
+++ src/main/java/com/study/component/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2021. Study
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.study.component.keypad;
+
+import com.study.component.CellNumberConverter;
+import com.study.model.Cell;
+
+/**
+ * * @author study
+ */
+public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
+
+    @Override
+    public Cell toCell(final char number) {
+        final int val = number - '0' - 1;
+        return new Cell(val / 3, val % 3);
+    }
+
+    @Override
+    public char toNumber(final Cell cell) {
+        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+    }
+}
