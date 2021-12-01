5c7c985ed55d270838e88d5bed459fc4891879c5
IlyaBuldakov <ggpanguin@gmail.com>
2021-12-01 14:11:05 +0400

    Simplify code with Lambda

--- src/main/java/com/study/component/DataPrinter.java
+++ src/main/java/com/study/component/DataPrinter.java
@@ -31,24 +31,27 @@ public class DataPrinter {
     }
 
     public void printMappingTable() {
+        print((i, j) -> String.valueOf(cellNumberConverter.toNumber(new Cell(i, j))));
+    }
+
+    public void printGameTable(final GameTable gameTable) {
+        print((i, j) -> String.valueOf(gameTable.getSign(new Cell(i, j))));
+    }
+
+    private void print(final Lambda lambda) {
         for (int i = 0; i < 3; i++) {
             System.out.println("-------------");
             for (int j = 0; j < 3; j++) {
-                System.out.print("| " + cellNumberConverter.toNumber(new Cell(i, j)) + " ");
+                System.out.print("| " + lambda.getValue(i, j) + " ");
             }
             System.out.println("|");
         }
         System.out.println("-------------");
     }
 
-    public void printGameTable(final GameTable gameTable) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println("-------------");
-            for (int j = 0; j < 3; j++) {
-                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
-            }
-            System.out.println("|");
-        }
-        System.out.println("-------------");
+
+    @FunctionalInterface
+    private interface Lambda {
+        String getValue(int i, int j);
     }
 }
