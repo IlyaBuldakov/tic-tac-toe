2ef2dafafdaf0f48b0055b9985ae9b6dc4199fb9
IlyaBuldakov <ggpanguin@gmail.com>
2021-12-03 00:04:39 +0400

    Implement GameFactory logic

--- src/main/java/com/study/component/GameFactory.java
+++ src/main/java/com/study/component/GameFactory.java
@@ -30,24 +30,60 @@ import static com.study.model.PlayerType.USER;
  */
 public class GameFactory {
 
-    private final PlayerType playerType1 = USER;
+    private final PlayerType player1Type;
 
-    private final PlayerType playerType2 = COMPUTER;
+    private final PlayerType player2Type;
 
     public GameFactory(final String[] args) {
-        // TODO
+        PlayerType player1Type = null;
+        PlayerType player2Type = null;
+        for (final String arg : args) {
+            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
+                if (player1Type == null) {
+                    player1Type = PlayerType.valueOf(arg.toUpperCase());
+                } else if (player2Type == null) {
+                    player2Type = PlayerType.valueOf(arg.toUpperCase());
+                } else {
+                    System.err.println("Unsupported command line argument: '" + arg + "'");
+                }
+            } else {
+                System.err.println("Unsupported command line argument: '" + arg + "'");
+            }
+        }
+        if (player1Type == null) {
+            this.player1Type = USER;
+            this.player2Type = COMPUTER;
+        } else if (player2Type == null) {
+            this.player1Type = USER;
+            this.player2Type = player1Type;
+        } else {
+            this.player1Type = player1Type;
+            this.player2Type = player2Type;
+        }
     }
 
     public Game create() {
         final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
-
+        final Player player1;
+        if (player1Type == USER) {
+            player1 = new Player(X, new UserMove(cellNumberConverter));
+        } else {
+            player1 = new Player(X, new ComputerMove());
+        }
+        final Player player2;
+        if (player2Type == USER) {
+            player2 = new Player(O, new UserMove(cellNumberConverter));
+        } else {
+            player2 = new Player(O, new ComputerMove());
+        }
+        final boolean canSecondPlayerMakeFirstMove = player1Type != player2Type;
         return new Game(
                 new DataPrinter(cellNumberConverter),
-                // FIXME
-                new Player(X, new ComputerMove()),
-                new Player(O, new ComputerMove()),
+                player1,
+                player2,
                 new WinnerVerifier(),
                 new CellVerifier(),
-                true);
+                canSecondPlayerMakeFirstMove
+        );
     }
 }
