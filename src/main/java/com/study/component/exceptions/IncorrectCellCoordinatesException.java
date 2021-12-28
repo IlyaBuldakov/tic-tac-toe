5c5a0062b5bc3c010cbb83fc9e4a91729c9e7afd
IlyaBuldakov <ggpanguin@gmail.com>
2021-12-29 01:17:44 +0400

    Add IncorrectCellCoordinatesException class

---
+++ src/main/java/com/study/component/exceptions/IncorrectCellCoordinatesException.java
@@ -0,0 +1,10 @@
+package com.study.component.exceptions;
+
+/**
+ * * @author study
+ */
+public class IncorrectCellCoordinatesException extends RuntimeException {
+    public IncorrectCellCoordinatesException(String message) {
+        super(message);
+    }
+}
