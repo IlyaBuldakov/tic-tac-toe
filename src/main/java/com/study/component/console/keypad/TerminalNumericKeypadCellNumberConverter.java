83085bb371ffa710865d3604983771747a79f519
IlyaBuldakov <ggpanguin@gmail.com>
2021-12-29 01:41:11 +0400

    Fix TerminalNumericKeypadCellNumberConverter class

--- src/main/java/com/study/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/main/java/com/study/component/console/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -17,7 +17,10 @@
 package com.study.component.console.keypad;
 
 import com.study.component.console.CellNumberConverter;
+import com.study.component.exceptions.IncorrectCellCoordinatesException;
 import com.study.model.game.Cell;
+
+import static java.lang.String.format;
 
 /**
  * * @author study
@@ -26,12 +29,26 @@ public class TerminalNumericKeypadCellNumberConverter implements CellNumberConve
 
     @Override
     public Cell toCell(final char number) {
-        final int val = number - '0' - 1;
-        return new Cell(val / 3, val % 3);
+        if (number >= '1' && number <= '9') {
+            final int val = number - '0' - 1;
+            return new Cell(val / 3, val % 3);
+        } else {
+            throw new IllegalArgumentException(
+                    format("Incorrect cell number. " +
+                            "Your number %s does not fall in the range from 1 to 9", number)
+            );
+        }
     }
 
     @Override
     public char toNumber(final Cell cell) {
-        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        if (cell.getRow() >= 0 && cell.getRow() <= 2 && cell.getCol() >= 0 && cell.getCol() <= 2) {
+            return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
+        } else {
+            throw new IncorrectCellCoordinatesException(
+                    format("Illegal cell row and column coordinates (index must to be from 0 to 2). " +
+                            "You trying to find cell with row: %s and col: %s", cell.getRow(), cell.getCol())
+            );
+        }
     }
 }
